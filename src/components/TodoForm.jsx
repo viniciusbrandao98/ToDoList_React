21f54bfddc53fb8402a10b467575a9591810a7c3
viniciusbrandao98 <viniciusbrandao9417@gmail.com>
2026-09-03 17:02:38 -0300

    feat: função criada handleSubmit para prevenir o recarregamento da página.

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import { useState } from 'react'
 
 const TodoForm = () => {
+  const [value, setValue] = useState('');
+  const [category, setCategory] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(value, category);
+
+  }
+
   return (
     <div className='todo-form'>
       <h2>Criar Tarefa:</h2>
-      <form>
-        <input type="text" placeholder='Adicione uma tarefa' />
-        <select>
-            <option value="">Selecione uma categoria</option>
-             <option value="Estudos">Estudos</option>
-              <option value="Trabalho">Trabalho</option>
-               <option value="Pessoal">Pessoal</option>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder='Adicione uma tarefa'
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <select onChange={(e) => setCategory(e.target.value)} >
+          <option value="">Selecione uma categoria</option>
+          <option value="Estudos">Estudos</option>
+          <option value="Trabalho">Trabalho</option>
+          <option value="Pessoal">Pessoal</option>
         </select>
         <button type="submit">Criar Tarefa</button>
       </form>
